c6f24a255f029862212845a66577d286e5347134
timkun92 <timnguyen@hotmail.co.uk>
2016-04-27 22:27:38 +0100

    Adding movement to the Boxes
    Added randomisation and movement using space key

--- js/CreatingBox.js
+++ js/CreatingBox.js
@@ -1,8 +1,91 @@
 
-function test()
+
+//Adding Images
+var iconBoxRedReady = false;
+var iconBoxRed = new Image();
+var iconBoxBlueReady = false;
+var iconBoxBlue = new Image();
+var iconBoxGreenReady = false;
+var iconBoxGreen = new Image();
+
+/*
+var box = {
+	movementSpeed: 20, x: 0, y: 100
+};
+*/
+
+iconBoxRed.src = "images/BoxRed.png";
+iconBoxBlue.src = "images/BoxBlue.png";
+iconBoxGreen.src = "images/BoxGreen.png";
+
+var boxList = [];
+
+iconBoxRed.onload = function() {
+	iconBoxRedReady = true;
+}
+	
+iconBoxBlue.onload = function() {
+	iconBoxBlueReady = true;
+}
+
+iconBoxGreen.onload = function() {
+	iconBoxGreenReady = true;
+}
+
+function AddingRandomBox()
+{
+	console.log("Adding Box");
+	
+	var randomInt = Math.floor((Math.random() * 3) + 1);
+	
+	var icon;
+		switch (randomInt)
+		{
+			case 1:
+				icon = iconBoxRed;
+				break;
+			case 2:
+				icon = iconBoxBlue;
+				break;
+			case 3:
+				icon = iconBoxGreen;
+				break;
+			default:
+				console.log("Unknown Number");
+		}
+	
+	var newBox = {
+		img: icon,
+		movementSpeed: 1,
+		id: randomInt,
+		x: 0,
+		y: 100
+	};
+	
+	boxList.push(newBox);
+	
+	console.log("Random Number: " + randomInt);
+	
+}
+
+function BoxMovement(m)
+{
+	for (var i = 0; i < boxList.length; i++)
+	{
+		boxList[i].x += boxList[i].movementSpeed;
+	}
+}
+
+function DrawBox(ctx)
 {
 	
-		console.log("Testing the script connected");
-		console.log("Second Testing")
-
+	
+	for (var b = 0; b < boxList.length; b++)
+	{
+		ctx.drawImage(boxList[b].img,boxList[b].x, boxList[b].y, 96, 96 );
+	}
 }
+
+
+
+
